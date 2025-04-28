8f116718be9b0414c0613231a726aa65e438ebe2
Neferpitou3871 <mhr_kura@outlook.com>
2025-04-28 13:41:02 +0200

    G-transformer

--- src/models/g_transformer.py
+++ src/models/g_transformer.py
@@ -12,20 +12,19 @@ import logging
 import mlflow
 from typing import List, Tuple
 
-from src.models.utils_lstm import VariationalLSTM
+from src.models.utils_transformer import AbsolutePositionalEncoding, RelativePositionalEncoding, TransformerMultiInputBlock
 from src.models.basic_blocks import OutcomeHead_GRNN
 
 logger = logging.getLogger(__name__)
 
 
-class PluginGCompNetwork(LightningModule):
+class G_transformer(LightningModule):
     """
-    Pytorch Lightning Module for the Regression adjusted G-computation CAPO learner with LSTM backbone.
+    Pytorch Lightning Module for the G-transformer https://github.com/konstantinhess/G_transformer
     The CAPO is estimated using the G-computation formula with a regression model for the potential outcomes.
     The regression model is a LSTM with variational dropout.
     We use \mu_t(h_t, a) =  E[Y_{t+m}^(do(a))|H_t=h_t] to represent the nuisance parameter of the potential outcome model
     For Plug-in learner, the CATE TE(h_t, a, b) is estimated as \mu_t(h_t, a) - \mu_t(h_t, b).
-    Implementation partly based on G-transformer https://github.com/konstantinhess/G_transformer
     """
     def __init__(self, args:DictConfig, treatment_sequence: np.ndarray = None):
         super().__init__()
@@ -60,31 +59,68 @@ class PluginGCompNetwork(LightningModule):
         self.fc_hidden_units = args.model.fc_hidden_size
         self.dropout_rate = args.model.dropout_rate
         self.num_layer = args.model.num_layer
+        self.num_heads = args.model.num_heads
+        self.pe_trainable = args.model.positional_encoding.trainable
+        self.relative_position = args.model.max_relative_position
+
+        self.head_size = self.seq_hidden_units // self.num_heads
+        
+        self.self_positional_encoding = RelativePositionalEncoding(self.relative_position, self.head_size, trainable = True)
+        self.self_positional_encoding_k = RelativePositionalEncoding(self.relative_position, self.head_size, trainable = True)
+        self.self_positional_encoding_v = RelativePositionalEncoding(self.relative_position, self.head_size, trainable = True)
 
         self.treatments_input_transformation = nn.Linear(self.dim_treatments, self.seq_hidden_units)
         self.vitals_input_transformation = nn.Linear(self.dim_vitals, self.seq_hidden_units)
-        self.outputs_input_transformation = nn.Linear(self.dim_outcome, self.seq_hidden_units)
+        self.output_input_transformation = nn.Linear(1, self.seq_hidden_units)
         self.static_input_transformation = nn.Linear(self.dim_static_features, self.seq_hidden_units)
 
-        self.lstm = VariationalLSTM(self.input_size, self.seq_hidden_units, self.num_layer, self.dropout_rate)
-
+        self.n_inputs = 3 if self.has_vitals else 2
+        self.transformer_blocks = nn.ModuleList([
+            TransformerMultiInputBlock(self.seq_hidden_units, 
+                                       self.num_heads, 
+                                       self.head_size, 
+                                       self.seq_hidden_units * 4,
+                                       self.dropout_rate,
+                                       self.dropout_rate,
+                                       self_positional_encoding_k=self.self_positional_encoding_k,
+                                       self_positional_encoding_v=self.self_positional_encoding_v,
+                                       n_inputs=self.n_inputs,
+                                       disable_cross_attention=False,
+                                       isolate_subnetwork='_')
+        ])
         self.hr_output_transformation = nn.Linear(self.seq_hidden_units, self.hr_size)
         self.output_dropout = nn.Dropout(self.dropout_rate)
 
+        # G-computation heads: nested expectations
         self.G_comp_heads = nn.ModuleList(
             [OutcomeHead_GRNN(self.seq_hidden_units, self.hr_size, self.fc_hidden_units, self.dim_treatments, 1)
              for _ in range(self.projection_horizon + 1)]
         )
 
+
     def build_hr(self, prev_treatments, vitals, prev_outputs, static_features, active_entries):
 
-        x = torch.cat((prev_treatments, prev_outputs.unsqueeze(-1)), dim=-1)
-        x = torch.cat((x, vitals), dim=-1) if self.has_vitals else x
-        x = torch.cat((x, static_features.unsqueeze(1).expand(-1, x.size(1), -1)), dim=-1)
-        x = self.lstm(x, init_states=None)
+        active_entries_treat_outcomes = torch.clone(active_entries)
+        active_entries_vitals = torch.clone(active_entries)
+        T = prev_treatments.size(1)
 
-        output = self.output_dropout(x)
-        hr = nn.ELU()(self.hr_output_transformation(output))
+        x_t = self.treatments_input_transformation(prev_treatments)
+        x_o = self.output_input_transformation(prev_outputs.unsqueeze(-1))
+        x_v = self.vitals_input_transformation(vitals) if self.has_vitals else None
+        x_s = self.static_input_transformation(static_features.unsqueeze(1)).expand(-1, T, -1)
+
+        for block in self.transformer_blocks:
+            if self.self_positional_encoding is not None:
+                x_t = x_t + self.self_positional_encoding(x_t)
+                x_o = x_o + self.self_positional_encoding(x_o)
+                x_v = x_v + self.self_positional_encoding(x_v) if self.has_vitals else None 
+            
+            x_t, x_o, x_v = block((x_t, x_o, x_v), x_s, active_entries_treat_outcomes, active_entries_vitals)
+
+        x = (x_t + x_o + x_v) / 3 if self.has_vitals else (x_t + x_o) / 2
+        x = self.output_dropout(x)
+        hr = nn.ELU()(self.hr_output_transformation(x))
+
         return hr
     
     def forward(self, batch, mode = 'test'):
